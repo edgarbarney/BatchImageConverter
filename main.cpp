5c13d37042a9f1a731e4b935fc7fe97bb142a564
FranticDreamer <edwardgmgmd@gmail.com>
2021-03-10 20:37:48 +0300

    Get Locale

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <Magick++.h>
 #include <QApplication>
+#include <locale.h>
 
 using namespace Magick;
 
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 	InitializeMagick(*argv);
+	setlocale(LC_ALL, "");
     MainWindow w;
     w.show();
     return a.exec();
